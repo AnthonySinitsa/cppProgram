6c0334cc639a2f45875735062fd1b5dc45ebc4de
Anton <itonyemail@gmail.com>
2024-08-13 14:45:33 -0700

    cout

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 int add(int x, int y) // integer version
 {
     return x + y;
@@ -10,5 +12,6 @@ double add(double x, double y) // floating point version
 
 int main()
 {
+    std::cout << add(1.5, 2.0) << "\n";
     return 0;
 }
