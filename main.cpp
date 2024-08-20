9a89f8dcc06776ce33ba82225dc0c217ad996bf3
Anton <itonyemail@gmail.com>
2024-08-19 21:21:25 -0700

    constrcturoes

--- main.cpp
+++ main.cpp
@@ -1,33 +1,24 @@
 #include <iostream>
 
-class Date
+class Something
 {
 private:
-    int m_year{ 2020 };
-    int m_month{ 10 };
-    int m_day{ 14 };
+    int m_x{};
 
 public:
-    void print()
+    Something() // constructors must be non-const
     {
-        std::cout << m_year << '/' << m_month << '/' << m_day << '\n';
+        m_x = 5; // okay to modify members in non-const constructor
     }
 
-    int getYear() const { return m_year; }        // getter for year
-    void setYear(int year) { m_year = year; }     // setter for year
-
-    int getMonth() const { return m_month; }     // getter for month
-    void setMonth(int month) { m_month = month; } // setter for month
-
-    int getDay() const { return m_day; }          // getter for day
-    void setDay(int day) { m_day = day; }         // setter for day
+    int getX() const { return m_x; } // const
 };
 
 int main()
 {
-    Date d{};
-    d.setYear(2021);
-    std::cout << "The year is: " << d.getYear() << '\n';
+    const Something s{}; // const object, implicitly invokes (non-const) constructor
+
+    std::cout << s.getX(); // prints 5
 
     return 0;
 }
